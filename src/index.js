77e13bb561c7e39c51060fd47f0ee9b961066a32
Fasilfaz <fazsby2001@gmail.com>
2024-11-13 19:42:10 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ dotenv.config({path: "./.env"});
 dbConnect();
 
 const port = process.env.PORT;
-
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 app.listen(port, () => {
     console.log(`server is up on port ${port}`)
 })
